e24401bb688ce6d4fcd8fcef8cb460f00b3971ff
John Godsey <johngodsey@gmail.com>
2019-03-13 20:23:22 -0400

    Adding config file and preparing for environment files.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -10,19 +12,49 @@ import (
 )
 
 func main() {
+	cluster := "default"
+	redisAddress := "localhost:6379"
+	redisPassword := ""
+
+	fBytes, err := ioutil.ReadFile(".config")
+	if err == nil {
+		var f interface{}
+		err2 := json.Unmarshal(fBytes, &f)
+		if err2 == nil {
+			m := f.(map[string]interface{})
+			redisAddress = m["redis-address"].(string)
+			redisPassword = m["redis-password"].(string)
+			cluster = m["cluster"].(string)
+		}
+	}
+
 	cmd := ""
 	if len(os.Args) > 0 {
 		cmd = os.Args[1]
 	}
 
 	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		Addr:     redisAddress,
+		Password: redisPassword, // no password set
+		DB:       0,             // use default DB
 	})
 
 	switch cmd {
 	case "":
+	case "purge":
+		fmt.Println("Sure? (Y)")
+		reader := bufio.NewReader(os.Stdin)
+		a, _, err := reader.ReadRune()
+		if err == nil {
+			if a == 'Y' {
+				keys := client.Keys(cluster + ":*").Val()
+				for k := range keys {
+					fmt.Println("Removed:", keys[k])
+					client.Del(keys[k])
+				}
+			}
+		}
+
 	case "threads":
 		cmd2 := ""
 		if len(os.Args) > 2 {
@@ -30,7 +62,7 @@ func main() {
 		}
 		switch cmd2 {
 		case "":
-			threads := client.Keys("default:Threads:*").Val()
+			threads := client.Keys(cluster + ":Threads:*").Val()
 			for i := range threads {
 				fmt.Println(threads[i])
 				fmt.Println("-", "Status", client.HGet(threads[i], "Status").Val())
@@ -62,17 +94,10 @@ func main() {
 
 				scriptArray := strings.Split(scripts, ",")
 				for i := range scriptArray {
-					scriptName := scriptArray[i]
-					fBytes, err := ioutil.ReadFile(scriptName)
+					err := loadScript(client, cluster, scriptArray[i])
 					if err != nil {
-						fmt.Println("File load failed")
-						return
+						fmt.Println("Failed to load: " + scriptArray[i])
 					}
-					client.HSet("default:Threads:"+scriptName, "Source", string(fBytes))
-					client.HSet("default:Threads:"+scriptName, "Status", "enabled")
-					client.HSet("default:Threads:"+scriptName, "State", "stopped")
-					client.HSet("default:Threads:"+scriptName, "Heartbeat", 0)
-					client.HSet("default:Threads:"+scriptName, "Owner", "")
 				}
 
 			}
@@ -85,7 +110,7 @@ func main() {
 		}
 		switch cmd2 {
 		case "":
-			threads := client.Keys("default:Endpoints:*").Val()
+			threads := client.Keys(cluster + ":Endpoints:*").Val()
 			for i := range threads {
 				fmt.Println(threads[i])
 				fmt.Println("-", "Status", client.HGet(threads[i], "Status").Val())
@@ -114,28 +139,24 @@ func main() {
 
 				scriptArray := strings.Split(scripts, ",")
 				for i := range scriptArray {
-					scriptName := scriptArray[i]
-					fBytes, err := ioutil.ReadFile(scriptName)
+					err := loadEndpoint(client, cluster, scriptArray[i], scriptArray[i])
 					if err != nil {
-						fmt.Println("File load failed")
-						return
+						fmt.Println("Failed to load script: " + scriptArray[i])
 					}
-					client.HSet("default:Endpoints:"+scriptName, "Source", string(fBytes))
-					client.HSet("default:Endpoints:"+scriptName, "Status", "enabled")
 				}
 
 			}
 		}
 
 	case "warts":
-
 		cmd2 := ""
 		if len(os.Args) > 2 {
 			cmd2 = os.Args[2]
 		}
 		switch cmd2 {
 		case "":
-			warts := client.Keys("default:Warts:*").Val()
+			//Print out warts
+			warts := client.Keys(cluster + ":Warts:*").Val()
 			for i := range warts {
 				s := strings.Split(warts[i], ":")
 				if s[len(s)-1] != "Health" {
@@ -152,10 +173,37 @@ func main() {
 			if len(os.Args) < 4 {
 				fmt.Println("invalid")
 			} else {
-				key := "default:Warts:" + os.Args[3]
-				client.HSet(key, "Status", "disabled")
+				stopWart(client, cluster, os.Args[3])
 			}
 
 		}
 	}
+}
+
+func stopWart(client *redis.Client, cluster string, wart string) {
+	key := cluster + ":Warts:" + wart
+	client.HSet(key, "Status", "disabled")
+}
+
+func loadEndpoint(client *redis.Client, cluster string, scriptName string, scriptPath string) (err error) {
+	fBytes, err := ioutil.ReadFile(scriptPath)
+	if err != nil {
+		return
+	}
+	client.HSet(cluster+":Endpoints:"+scriptName, "Source", string(fBytes))
+	client.HSet(cluster+":Endpoints:"+scriptName, "Status", "enabled")
+	return
+}
+
+func loadScript(client *redis.Client, cluster string, scriptName string) (err error) {
+	fBytes, err := ioutil.ReadFile(scriptName)
+	if err != nil {
+		return
+	}
+	client.HSet(cluster+":Threads:"+scriptName, "Source", string(fBytes))
+	client.HSet(cluster+":Threads:"+scriptName, "Status", "enabled")
+	client.HSet(cluster+":Threads:"+scriptName, "State", "stopped")
+	client.HSet(cluster+":Threads:"+scriptName, "Heartbeat", 0)
+	client.HSet(cluster+":Threads:"+scriptName, "Owner", "")
+	return
 }
